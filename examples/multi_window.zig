b1aa12b633e73b59cfeefab1f1e2aa3e44ff0123
geemili <opensource@geemili.xyz>
2024-09-09 23:14:08 -0600

    feat: vulkan: get multi_window example running
    TODO: figure out why the windows mess with each other

--- examples/multi_window.zig
+++ examples/multi_window.zig
@@ -1,9 +1,13 @@
 pub const main = seizer.main;
 
+var gfx: seizer.Graphics = undefined;
 var canvas: seizer.Canvas = undefined;
 var next_window_id: usize = 1;
 
 pub fn init() !void {
+    gfx = try seizer.platform.createGraphics(seizer.platform.allocator(), .{});
+    errdefer gfx.destroy();
+
     seizer.platform.setEventCallback(onEvent);
 
     _ = try seizer.platform.createWindow(.{
@@ -11,7 +15,7 @@ pub fn init() !void {
         .on_render = render,
     });
 
-    canvas = try seizer.Canvas.init(seizer.platform.allocator(), .{});
+    canvas = try seizer.Canvas.init(seizer.platform.allocator(), gfx, .{});
     errdefer canvas.deinit();
 
     seizer.platform.setDeinitCallback(deinit);
@@ -19,6 +23,7 @@ pub fn init() !void {
 
 pub fn deinit() void {
     canvas.deinit();
+    gfx.destroy();
 }
 
 fn onEvent(event: seizer.input.Event) anyerror!void {
@@ -42,19 +47,26 @@ fn onEvent(event: seizer.input.Event) anyerror!void {
 }
 
 fn render(window: seizer.Window) !void {
-    gl.clearColor(0.7, 0.5, 0.5, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    const cmd_buf = try gfx.begin(.{
+        .size = window.getSize(),
+        .clear_color = .{ 0.7, 0.5, 0.5, 1.0 },
+    });
 
-    const c = canvas.begin(.{
+    const c = canvas.begin(cmd_buf, .{
         .window_size = window.getSize(),
-        .framebuffer_size = window.getFramebufferSize(),
     });
-    _ = c.writeText(.{ window.getSize()[0] / 2, window.getSize()[1] / 2 }, "Press N to spawn new window", .{
-        .@"align" = .center,
-    });
-    canvas.end();
 
-    try window.swapBuffers();
+    const window_size = [2]f32{ @floatFromInt(window.getSize()[0]), @floatFromInt(window.getSize()[1]) };
+
+    _ = c.writeText(.{ window_size[0] / 2, window_size[1] / 2 }, "Press N to spawn new window", .{
+        .scale = 3,
+        .@"align" = .center,
+        .baseline = .middle,
+    });
+
+    canvas.end(cmd_buf);
+
+    try window.presentFrame(try cmd_buf.end());
 }
 
 pub fn nWindowDeinit(window: seizer.Window) void {
@@ -65,23 +77,27 @@ pub fn nWindowDeinit(window: seizer.Window) void {
 fn nWindowRender(window: seizer.Window) !void {
     const title: [*:0]u8 = @ptrCast(window.getUserdata());
 
-    gl.clearColor(0, 0, 0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    const cmd_buf = try gfx.begin(.{
+        .size = window.getSize(),
+        .clear_color = .{ 0.7, 0.5, 0.5, 1.0 },
+    });
 
-    const c = canvas.begin(.{
+    const c = canvas.begin(cmd_buf, .{
         .window_size = window.getSize(),
-        .framebuffer_size = window.getFramebufferSize(),
     });
 
-    _ = c.writeText(.{ window.getSize()[0] / 2, window.getSize()[1] / 2 }, std.mem.span(title), .{
+    const window_size = [2]f32{ @floatFromInt(window.getSize()[0]), @floatFromInt(window.getSize()[1]) };
+
+    _ = c.writeText(.{ window_size[0] / 2, window_size[1] / 2 }, std.mem.span(title), .{
+        .scale = 3,
         .@"align" = .center,
+        .baseline = .middle,
     });
 
-    canvas.end();
+    canvas.end(cmd_buf);
 
-    try window.swapBuffers();
+    try window.presentFrame(try cmd_buf.end());
 }
 
 const seizer = @import("seizer");
-const gl = seizer.gl;
 const std = @import("std");
