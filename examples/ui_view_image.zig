a0222ccf392dc29ba7d9764e1b1896c575810fb9
geemili <opensource@geemili.xyz>
2024-09-11 09:13:22 -0600

    fix: example: update ui_image_view to current API

--- examples/ui_view_image.zig
+++ examples/ui_view_image.zig
@@ -1,26 +1,39 @@
 pub const main = seizer.main;
 
+var gfx: seizer.Graphics = undefined;
 var canvas: seizer.Canvas = undefined;
-var ui_texture: seizer.Texture = undefined;
-var character_texture: seizer.Texture = undefined;
+var ui_texture: *seizer.Graphics.Texture = undefined;
+var character_texture: *seizer.Graphics.Texture = undefined;
 var stage: *seizer.ui.Stage = undefined;
 
 pub fn init() !void {
     seizer.platform.setEventCallback(onEvent);
+
+    gfx = try seizer.platform.createGraphics(seizer.platform.allocator(), .{});
+    errdefer gfx.destroy();
+
     _ = try seizer.platform.createWindow(.{
         .title = "UI Stage - Seizer Example",
         .on_render = render,
         .on_destroy = deinit,
     });
 
-    canvas = try seizer.Canvas.init(seizer.platform.allocator(), .{});
+    canvas = try seizer.Canvas.init(seizer.platform.allocator(), gfx, .{});
     errdefer canvas.deinit();
 
-    ui_texture = try seizer.Texture.initFromFileContents(seizer.platform.allocator(), @embedFile("./assets/ui.png"), .{});
-    errdefer ui_texture.deinit();
+    var ui_image = try seizer.zigimg.Image.fromMemory(seizer.platform.allocator(), @embedFile("./assets/ui.png"));
+    defer ui_image.deinit();
 
-    character_texture = try seizer.Texture.initFromFileContents(seizer.platform.allocator(), @embedFile("./assets/wedge.png"), .{});
-    errdefer character_texture.deinit();
+    const ui_image_size = [2]u32{ @intCast(ui_image.width), @intCast(ui_image.height) };
+
+    ui_texture = try gfx.createTexture(ui_image, .{});
+    errdefer gfx.destroyTexture(ui_texture);
+
+    var character_image = try seizer.zigimg.Image.fromMemory(seizer.platform.allocator(), @embedFile("./assets/wedge.png"));
+    defer character_image.deinit();
+
+    character_texture = try gfx.createTexture(character_image, .{});
+    errdefer gfx.destroyTexture(character_texture);
 
     stage = try seizer.ui.Stage.create(seizer.platform.allocator(), .{
         .padding = .{
@@ -30,7 +43,7 @@ pub fn init() !void {
         .text_font = &canvas.font,
         .text_scale = 1,
         .text_color = [4]u8{ 0xFF, 0xFF, 0xFF, 0xFF },
-        .background_image = seizer.NinePatch.initv(ui_texture, .{ .pos = .{ 0, 0 }, .size = .{ 48, 48 } }, .{ 16, 16 }),
+        .background_image = seizer.NinePatch.initv(ui_texture, ui_image_size, .{ .pos = .{ 0, 0 }, .size = .{ 48, 48 } }, .{ 16, 16 }),
         .background_color = [4]u8{ 0xFF, 0xFF, 0xFF, 0xFF },
     });
     errdefer stage.destroy();
@@ -54,7 +67,7 @@ pub fn init() !void {
     defer title_label.element().release();
     title_label.style = stage.default_style.with(.{
         .text_color = .{ 0x00, 0x00, 0x00, 0xFF },
-        .background_image = seizer.NinePatch.initv(ui_texture, .{ .pos = .{ 48, 0 }, .size = .{ 48, 48 } }, .{ 16, 16 }),
+        .background_image = seizer.NinePatch.initv(ui_texture, ui_image_size, .{ .pos = .{ 48, 0 }, .size = .{ 48, 48 } }, .{ 16, 16 }),
     });
     try frame_flexbox.appendChild(title_label.element());
 
@@ -66,11 +79,11 @@ pub fn init() !void {
     defer pan_zoom_flexbox.element().release();
     try pan_zoom.appendChild(pan_zoom_flexbox.element());
 
-    const character_image_element = try seizer.ui.Element.Image.create(stage, character_texture);
+    const character_image_element = try seizer.ui.Element.Image.create(stage, character_texture, .{ @intCast(character_image.width), @intCast(character_image.height) });
     defer character_image_element.element().release();
     try pan_zoom_flexbox.appendChild(character_image_element.element());
 
-    const image_element = try seizer.ui.Element.Image.create(stage, ui_texture);
+    const image_element = try seizer.ui.Element.Image.create(stage, ui_texture, ui_image_size);
     defer image_element.element().release();
     try pan_zoom_flexbox.appendChild(image_element.element());
 
@@ -82,9 +95,10 @@ pub fn init() !void {
 pub fn deinit(window: seizer.Window) void {
     _ = window;
     stage.destroy();
-    character_texture.deinit();
-    ui_texture.deinit();
+    gfx.destroyTexture(character_texture);
+    gfx.destroyTexture(ui_texture);
     canvas.deinit();
+    gfx.destroy();
 }
 
 fn onEvent(event: seizer.input.Event) !void {
@@ -94,22 +108,24 @@ fn onEvent(event: seizer.input.Event) !void {
 }
 
 fn render(window: seizer.Window) !void {
-    gl.clearColor(0.7, 0.5, 0.5, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    const c = canvas.begin(.{
-        .window_size = window.getSize(),
-        .framebuffer_size = window.getFramebufferSize(),
+    const cmd_buf = try gfx.begin(.{
+        .size = window.getSize(),
+        .clear_color = .{ 0.7, 0.5, 0.5, 1.0 },
     });
 
+    const c = canvas.begin(cmd_buf, .{
+        .window_size = window.getSize(),
+    });
+
+    const window_size = [2]f32{ @floatFromInt(window.getSize()[0]), @floatFromInt(window.getSize()[1]) };
+
     stage.needs_layout = true;
-    stage.render(c, window.getSize());
+    stage.render(c, window_size);
 
-    canvas.end();
+    canvas.end(cmd_buf);
 
-    try window.swapBuffers();
+    try window.presentFrame(try cmd_buf.end());
 }
 
 const seizer = @import("seizer");
-const gl = seizer.gl;
 const std = @import("std");
